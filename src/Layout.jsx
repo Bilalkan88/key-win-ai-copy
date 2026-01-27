f333754117127b9f64a7d1c4521201f63dc8cdf3
mykdpdesign <mykdpdesign@gmail.com>
2026-01-27 21:59:50 +0000

    File changes

---
+++ src/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Toaster } from 'sonner';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      {children}
+      <Toaster position="top-center" richColors />
+    </>
+  );
+}
