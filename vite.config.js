d98b7888fb398046cac6432b02dc0e34d6289551
Mini <mini@Minis-Mac-mini.local>
2026-05-23 12:34:40 +0300

    fix: resolve Chrome console warnings about unused preloaded resources

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
     viteCompression({ algorithm: 'brotliCompress', ext: '.br' }),
   ],
   build: {
+    modulePreload: false,
     rollupOptions: {
       output: {
         manualChunks(id) {
